ccc830c01de9d1bdf056768b78edb7e88fd6a6bb
RogerioAguilera <raguileraribeiro33@gmail.com>
2024-08-25 09:30:53 -0400

    test 02

--- .deps/remix-tests/DioCoin.sol
+++ .deps/remix-tests/DioCoin.sol
@@ -1,225 +1,73 @@
-// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
 
-pragma solidity >=0.4.22 <0.9.0;
+interface IERC20{
 
-library Assert {
+    //getters
+    function totalSupply() external view returns(uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
 
-  event AssertionEvent(
-    bool passed,
-    string message,
-    string methodName
-  );
+    //functions
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
-  event AssertionEventUint(
-    bool passed,
-    string message,
-    string methodName,
-    uint256 returned,
-    uint256 expected
-  );
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256);
 
-  event AssertionEventInt(
-    bool passed,
-    string message,
-    string methodName,
-    int256 returned,
-    int256 expected
-  );
+}
 
-  event AssertionEventBool(
-    bool passed,
-    string message,
-    string methodName,
-    bool returned,
-    bool expected
-  );
+contract DIOToken is IERC20{
 
-  event AssertionEventAddress(
-    bool passed,
-    string message,
-    string methodName,
-    address returned,
-    address expected
-  );
+    string public constant name = "DIO Token";
+    string public constant symbol = "DIO";
+    uint8 public constant decimals = 18;
 
-  event AssertionEventBytes32(
-    bool passed,
-    string message,
-    string methodName,
-    bytes32 returned,
-    bytes32 expected
-  );
+    mapping (address => uint256) balances;
 
-  event AssertionEventString(
-    bool passed,
-    string message,
-    string methodName,
-    string returned,
-    string expected
-  );
+    mapping(address => mapping(address=>uint256)) allowed;
 
-  event AssertionEventUintInt(
-    bool passed,
-    string message,
-    string methodName,
-    uint256 returned,
-    int256 expected
-  );
+    uint256 totalSupply_ = 10 ether;
 
-  event AssertionEventIntUint(
-    bool passed,
-    string message,
-    string methodName,
-    int256 returned,
-    uint256 expected
-  );
-
-  function ok(bool a, string memory message) public returns (bool result) {
-    result = a;
-    emit AssertionEvent(result, message, "ok");
-  }
-
-  function equal(uint256 a, uint256 b, string memory message) public returns (bool result) {
-    result = (a == b);
-    emit AssertionEventUint(result, message, "equal", a, b);
-  }
-
-  function equal(int256 a, int256 b, string memory message) public returns (bool result) {
-    result = (a == b);
-    emit AssertionEventInt(result, message, "equal", a, b);
-  }
-
-  function equal(bool a, bool b, string memory message) public returns (bool result) {
-    result = (a == b);
-    emit AssertionEventBool(result, message, "equal", a, b);
-  }
-
-  // TODO: only for certain versions of solc
-  //function equal(fixed a, fixed b, string message) public returns (bool result) {
-  //  result = (a == b);
-  //  emit AssertionEvent(result, message);
-  //}
-
-  // TODO: only for certain versions of solc
-  //function equal(ufixed a, ufixed b, string message) public returns (bool result) {
-  //  result = (a == b);
-  //  emit AssertionEvent(result, message);
-  //}
-
-  function equal(address a, address b, string memory message) public returns (bool result) {
-    result = (a == b);
-    emit AssertionEventAddress(result, message, "equal", a, b);
-  }
-
-  function equal(bytes32 a, bytes32 b, string memory message) public returns (bool result) {
-    result = (a == b);
-    emit AssertionEventBytes32(result, message, "equal", a, b);
-  }
-
-  function equal(string memory a, string memory b, string memory message) public returns (bool result) {
-     result = (keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b)));
-     emit AssertionEventString(result, message, "equal", a, b);
-  }
-
-  function notEqual(uint256 a, uint256 b, string memory message) public returns (bool result) {
-    result = (a != b);
-    emit AssertionEventUint(result, message, "notEqual", a, b);
-  }
-
-  function notEqual(int256 a, int256 b, string memory message) public returns (bool result) {
-    result = (a != b);
-    emit AssertionEventInt(result, message, "notEqual", a, b);
-  }
-
-  function notEqual(bool a, bool b, string memory message) public returns (bool result) {
-    result = (a != b);
-    emit AssertionEventBool(result, message, "notEqual", a, b);
-  }
-
-  // TODO: only for certain versions of solc
-  //function notEqual(fixed a, fixed b, string message) public returns (bool result) {
-  //  result = (a != b);
-  //  emit AssertionEvent(result, message);
-  //}
-
-  // TODO: only for certain versions of solc
-  //function notEqual(ufixed a, ufixed b, string message) public returns (bool result) {
-  //  result = (a != b);
-  //  emit AssertionEvent(result, message);
-  //}
-
-  function notEqual(address a, address b, string memory message) public returns (bool result) {
-    result = (a != b);
-    emit AssertionEventAddress(result, message, "notEqual", a, b);
-  }
-
-  function notEqual(bytes32 a, bytes32 b, string memory message) public returns (bool result) {
-    result = (a != b);
-    emit AssertionEventBytes32(result, message, "notEqual", a, b);
-  }
-
-  function notEqual(string memory a, string memory b, string memory message) public returns (bool result) {
-    result = (keccak256(abi.encodePacked(a)) != keccak256(abi.encodePacked(b)));
-    emit AssertionEventString(result, message, "notEqual", a, b);
-  }
-
-  /*----------------- Greater than --------------------*/
-  function greaterThan(uint256 a, uint256 b, string memory message) public returns (bool result) {
-    result = (a > b);
-    emit AssertionEventUint(result, message, "greaterThan", a, b);
-  }
-
-  function greaterThan(int256 a, int256 b, string memory message) public returns (bool result) {
-    result = (a > b);
-    emit AssertionEventInt(result, message, "greaterThan", a, b);
-  }
-  // TODO: safely compare between uint and int
-  function greaterThan(uint256 a, int256 b, string memory message) public returns (bool result) {
-    if(b < int(0)) {
-      // int is negative uint "a" always greater
-      result = true;
-    } else {
-      result = (a > uint(b));
+    constructor(){
+        balances[msg.sender] = totalSupply_;
     }
-    emit AssertionEventUintInt(result, message, "greaterThan", a, b);
-  }
-  function greaterThan(int256 a, uint256 b, string memory message) public returns (bool result) {
-    if(a < int(0)) {
-      // int is negative uint "b" always greater
-      result = false;
-    } else {
-      result = (uint(a) > b);
-    }
-    emit AssertionEventIntUint(result, message, "greaterThan", a, b);
-  }
-  /*----------------- Lesser than --------------------*/
-  function lesserThan(uint256 a, uint256 b, string memory message) public returns (bool result) {
-    result = (a < b);
-    emit AssertionEventUint(result, message, "lesserThan", a, b);
-  }
 
-  function lesserThan(int256 a, int256 b, string memory message) public returns (bool result) {
-    result = (a < b);
-    emit AssertionEventInt(result, message, "lesserThan", a, b);
-  }
-  // TODO: safely compare between uint and int
-  function lesserThan(uint256 a, int256 b, string memory message) public returns (bool result) {
-    if(b < int(0)) {
-      // int is negative int "b" always lesser
-      result = false;
-    } else {
-      result = (a < uint(b));
+    function totalSupply() public override view returns (uint256) {
+        return totalSupply_;
     }
-    emit AssertionEventUintInt(result, message, "lesserThan", a, b);
-  }
 
-  function lesserThan(int256 a, uint256 b, string memory message) public returns (bool result) {
-    if(a < int(0)) {
-      // int is negative int "a" always lesser
-      result = true;
-    } else {
-      result = (uint(a) < b);
+    function balanceOf(address tokenOwner) public override view returns (uint256){
+        return balances[tokenOwner];
     }
-    emit AssertionEventIntUint(result, message, "lesserThan", a, b);
-  }
+
+    function transfer(address receiver, uint256 numTokens) public override returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender]-numTokens;
+        balances[receiver] = balances[receiver]+numTokens;
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(address delegate, uint256 numTokens) public override returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate) public override view returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+
+        balances[owner] = balances[owner]-numTokens;
+        allowed[owner][msg.sender] = allowed[owner][msg.sender]-numTokens;
+        balances[buyer] = balances[buyer]+numTokens;
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+
 }
